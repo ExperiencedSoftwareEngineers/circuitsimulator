6c418626f5299f090cb5124d792929704752c50c
pu19 <pu19@imperial.ac.uk>
2020-06-03 01:15:07 +0300

    I dont even know if it works or not but it is past 1am so whatever

--- netlist_parse.cpp
+++ netlist_parse.cpp
@@ -46,13 +46,69 @@ vector<string> troy()
 
 int main(){
     vector<string> line;
+	Network chad;
     while(!cin.fail()){
-        line 
-        /*vector<string> cabbage = troy();
+        line = troy();
+	if (line[0][0] == 'R')
+	{
+		if('0'!=line[1][0])
+		int node1 = stoi(line[1].substr(1));
+		else node1 = 0;
+		if('0'!=line[1][0])
+		int node2 = stoi(line[1].substr(1));
+		else node2 = 0;
+
+		vector<int> inputnodes{node1,node2};
+		Component lol = Resistor(line[0], inputnodes, line[3]);
+
+		chad.parts.push_back(lol);
+        }
+	if (line[0][0] == 'L')
+	{
+		if('0'!=line[1][0])
+		int node1 = stoi(line[1].substr(1));
+		else node1 = 0;
+		if('0'!=line[1][0])
+		int node2 = stoi(line[1].substr(1));
+		else node2 = 0;
+
+		vector<int> inputnodes{node1,node2};
+		Component lol = Inductor(line[0], inputnodes, line[3]);
+
+		chad.parts.push_back(lol);
+        }
+	if (line[0][0] == 'C')
+	{
+		if('0'!=line[1][0])
+		int node1 = stoi(line[1].substr(1));
+		else node1 = 0;
+		if('0'!=line[1][0])
+		int node2 = stoi(line[1].substr(1));
+		else node2 = 0;
+
+		vector<int> inputnodes{node1,node2};
+		Component lol = Capacitor(line[0], inputnodes, line[3]);
+
+		chad.parts.push_back(lol);
+        }
+	if (line[0][0] == 'V')
+	{
+		if('0'!=line[1][0])
+		int node1 = stoi(line[1].substr(1));
+		else node1 = 0;
+		if('0'!=line[1][0])
+		int node2 = stoi(line[1].substr(1));
+		else node2 = 0;
+
+		vector<int> inputnodes{node1,node2};
+		Component lol = Voltage(line[0], inputnodes, stoi(line[3].substr(5)), line[5], stoi(line[5].substr(0,(line[5].length())-1);
+
+		chad.parts.push_back(lol);
+        }
+    }
+}/*vector<string> cabbage = troy();
         
         for (int i = 0; i < cabbage.size(); i++)
         {
             cout<< cabbage[i] << endl;
         }*/
-    }
-}
--- network.hpp
+++ network.hpp
@@ -11,34 +11,79 @@
 
 using namespace std;
 
-//class Network{
-//    vector<Component> parts;
-//};
+class Network{
+	public:
+    vector<Component> parts;
+	float stop; //stop time of the transient analysis
+	float step; //time between intervals
+	
+};
 
 class Component{
+	public:
     string name; // name, e.g. R1, C3, etc.
     vector<int> nodes; // vector containing the 2 nodes of the component
 };
 
 class Resistor : public Component {
+public:
+	Resistor(string na, vector<int> no, float val)
+	{
+		name = na;
+		nodes = no;
+		value = val;
+	}
+
     float value;
 };
 
 class Inductor : public Component {
+public:
+ 	Inductor(string na, vector<int> no, float val)
+	{
+		name = na;
+		nodes = no;
+		value = val;
+	}
     float value;
 };
 
 class Capacitor : public Component {
+public:
+	Capacitor(string na, vector<int> no, float val)
+	{
+		name = na;
+		nodes = no;
+		value = val;
+	}
     float value;
 };
 
 class Voltage : public Component {
+public:
+	Voltage(string na, vector<int> no, float amp, float fre, float off)
+	{
+		name = na;
+		nodes = no;
+		amplitude = amp;
+		frequency = fre;
+		offset = off;
+	}
     float amplitude;
     float frequency;
     float offset;
 };
 
 class Current : public Component {
+public:
+	Current(string na, vector<int> no, float amp, float fre, float off)
+	{
+		name = na;
+		nodes = no;
+		amplitude = amp;
+		frequency = fre;
+		offset = off;
+	}
     float amplitude;
     float frequency;
     float offset;
