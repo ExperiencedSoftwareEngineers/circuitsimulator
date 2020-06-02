03128a4163ddea5652cd362b95613d7383d6c125
jha19 <jha19@imperial.ac.uk>
2020-06-02 19:49:14 +0100

    Basic header started.

---
+++ network.hpp
@@ -0,0 +1,45 @@
+#ifndef network_hpp
+
+#define network_hpp
+
+#include <vector>
+#include <string>
+#include <complex>
+#include <iostream>
+#include <cmath>
+#include <cassert>
+
+using namespace std;
+
+class Network{}
+
+class Component{
+    string name; // name, e.g. R1, C3, etc.
+    vector<int> nodes; // vector containing the 2 nodes of the component
+}
+
+class Resistor : public Component {
+    float value;
+};
+class Inductor : public Component {
+    float value;
+};
+class Capacitor : public Component {
+    float value;
+};
+class Voltage : public Component {
+    float amplitude;
+    float frequency;
+    float offset;
+};
+class Current : public Component {
+    float amplitude;
+    float frequency;
+    float offset;
+};
+
+//not compulsory bc it's *"advanced"*
+class Diode : public Component {};
+class Transistor : public Component {};
+
+
