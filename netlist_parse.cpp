3c398a3ec07073e52af5d55294909cac4d516c1e
jha19 <jha19@imperial.ac.uk>
2020-06-02 21:51:51 +0100

    Can successfully grab component into vector.

--- netlist_parse.cpp
+++ netlist_parse.cpp
@@ -1,12 +1,24 @@
-#include "netlist_parse.cpp"
+// #include "network.hpp"
+
+#include <vector>
+#include <string>
+#include <complex>
+#include <iostream>
+#include <cmath>
+#include <cassert>
+
+using namespace std;
 
 //Splits a single component line into individual vectors
-vector<string> next()
-{
+vector<string> troy()
+{   
+
     string line;
+
     vector<string> vector_line;
 
-    getline(src, line);
+    getline(cin, line);
+
 
     line += ' ';
     int prevBreak = 0;
@@ -15,7 +27,8 @@ vector<string> next()
     {
         if (line[i] == ' ' || line[i] == '\n')
         {
-            string word;
+            string word = "";
+
 
             for (int z = prevBreak; z < i; z++)
             {
@@ -29,4 +42,14 @@ vector<string> next()
     }
 
     return vector_line;
+}
+
+int main(){
+
+    vector<string> cabbage = troy();
+    
+    for (int i = 0; i < cabbage.size(); i++)
+     {
+        cout<< cabbage[i] << endl;
+     }
 }
--- network.hpp
+++ network.hpp
@@ -11,41 +11,41 @@
 
 using namespace std;
 
-class Network{
-    vector<Component> parts;
-}
+//class Network{
+//    vector<Component> parts;
+//};
 
 class Component{
     string name; // name, e.g. R1, C3, etc.
     vector<int> nodes; // vector containing the 2 nodes of the component
-}
+};
 
 class Resistor : public Component {
     float value;
-}
+};
 
 class Inductor : public Component {
     float value;
-}
+};
 
 class Capacitor : public Component {
     float value;
-}
+};
 
 class Voltage : public Component {
     float amplitude;
     float frequency;
     float offset;
-}
+};
 
 class Current : public Component {
     float amplitude;
     float frequency;
     float offset;
-}
+};
 
 //not compulsory bc it's *"advanced"*
 class Diode : public Component {};
 class Transistor : public Component {};
 
-
+#endif
